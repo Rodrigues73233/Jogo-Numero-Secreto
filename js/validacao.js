79fd558eb7276d93ba20ae677b067b4654417299
Gabriel Rodrigues <102063081+Rodrigues73233@users.noreply.github.com>
2023-01-16 14:29:12 -0300

    Adicionando funcionalidade ao Dizer "Game Over"

--- js/validacao.js
+++ js/validacao.js
@@ -2,8 +2,18 @@ function verificaSeOChutePossuiUmValorValido(chute) {
   const numero = +chute
 
   if (chuteForInvalido(numero)) {
-    elementoChute.innerHTML += '<div>Valor Invalido</div>'
-    return
+    if (chute.toUpperCase() === "GAME OVER") {
+      document.body.innerHTML =
+      `
+      <h2>Game Over!!!</h2>
+      <h3>Pressione o botão para jogar novamente</h3>
+      <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+      `
+      document.body.style.backgroundColor = "black";
+    }
+    else {
+      elementoChute.innerHTML += '<div>Valor Invalido</div>'
+    }
   }
 
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
